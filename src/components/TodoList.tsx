bf66223fd1dc9b1bcef98740317b0949c4e73fb1
Sergei Koshelnikov <kosheklnikovsa@gmail.com>
2024-04-10 15:26:23 +0200

    update logic

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -14,7 +14,7 @@ export const TodoList = () => {
             <h1>My Todos:</h1>
             <button onClick={() => {
                 axios.post(`http://localhost:3000/todos/`, {
-                    type: 'solid',
+                    edit: 'allow',
                     title: "New Todo Item"
                 })
                     .then(() => {
@@ -31,11 +31,10 @@ export const TodoList = () => {
                 {todos.map(todo => (
                     <li key={todo.id}>
                         <span>{todo.title}</span>
-                        {todo.type === 'solid' && (
+                        {todo.edit === 'allow' && (
                             <>
-                                <span>SOLID</span>
                                 <button onClick={() => {
-                                    axios.patch(`http://localhost:3000/todos/${todo.id}`, {type: 'kiss'})
+                                    axios.patch(`http://localhost:3000/todos/${todo.id}`, {edit: 'disallow'})
                                         .then(() => {
                                             axios.get('http://localhost:3000/todos')
                                                 .then(({data}) => setTodos(data));
@@ -45,29 +44,7 @@ export const TodoList = () => {
                                         <path d="M20.1497 7.93997L8.27971 19.81C7.21971 20.88 4.04971 21.3699 3.27971 20.6599C2.50971 19.9499 3.06969 16.78 4.12969 15.71L15.9997 3.84C16.5478 3.31801 17.2783 3.03097 18.0351 3.04019C18.7919 3.04942 19.5151 3.35418 20.0503 3.88938C20.5855 4.42457 20.8903 5.14781 20.8995 5.90463C20.9088 6.66146 20.6217 7.39189 20.0997 7.93997H20.1497Z" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
                                         <path d="M21 21H12" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
                                     </svg>
-                                    <span>Set type to KISS</span></button>
-                            </>
-                        )}
-
-                        {todo.type === 'kiss' && (
-                            <>
-                                <span>KISS</span>
-                                <button onClick={() => {
-                                    axios.patch(`http://localhost:3000/todos/${todo.id}`, {type: 'solid'})
-                                        .then(() => {
-                                            axios.get('http://localhost:3000/todos')
-                                                .then(({data}) => setTodos(data));
-                                        });
-
-                                }}>
-                                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                        <path d="M20.1497 7.93997L8.27971 19.81C7.21971 20.88 4.04971 21.3699 3.27971 20.6599C2.50971 19.9499 3.06969 16.78 4.12969 15.71L15.9997 3.84C16.5478 3.31801 17.2783 3.03097 18.0351 3.04019C18.7919 3.04942 19.5151 3.35418 20.0503 3.88938C20.5855 4.42457 20.8903 5.14781 20.8995 5.90463C20.9088 6.66146 20.6217 7.39189 20.0997 7.93997H20.1497Z" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-                                        <path d="M21 21H12" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-                                    </svg>
-
-
-                                    <span>Set type to SOLID</span>
-                                </button>
+                                    <span>Disallow</span></button>
                             </>
                         )}
 
